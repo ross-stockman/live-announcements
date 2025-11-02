c34ff92477715c7da6a0bb5e934ef5a70fef2ea3
ross-stockman <ross.stockman@gmail.com>
2025-11-02 17:11:49 -0600

    refactoring

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import AnnouncementsPage from "./components/AnnouncementsPage.tsx";
+
 function App() {
 
   return (
     <>
-      <h1>Hello World!</h1>
+      <AnnouncementsPage />
     </>
   )
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import AnnouncementsPage from './components/AnnouncementsPage';
+import './index.css'
+import App from "./App.tsx";
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <AnnouncementsPage />
+        <App />
     </React.StrictMode>
 );
